48c2222e5484bb737aea8738c8695c5f7334acfb
student14 <student14@student14s-macbook-air.local>
2019-02-21 18:50:46 -0600

    Initial Commit

---
+++ Fibonacci/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Fibonacci
+//
+//  Created by student14 on 2/21/19.
+//  Copyright © 2019 student14. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
